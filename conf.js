4fa8e3e69a21e8fc4d6a2d8ba310cdbe69494553
Howard T. Chiam <hchiam@users.noreply.github.com>
2018-12-18 22:23:15 -0500

    Create conf.js

---
+++ conf.js
@@ -0,0 +1,5 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['todo-spec.js']
+};
+// You can use port 8000 if you use: webdriver-manager start --seleniumPort 8000
